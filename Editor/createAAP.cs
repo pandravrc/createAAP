3b08c2c84e9bfdd6640fdbd5fb6fa59398b062f7
pandravrc <162582491+pandravrc@users.noreply.github.com>
2024-03-17 11:50:06 +0900

    Initial commit

---
+++ Editor/createAAP.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class AAP : MonoBehaviour
+{
+    [MenuItem("Pan/Sample_CreateAAP")]
+    private static void Sample_CreateAAP()
+    {
+        Debug.Log(createAAP("SampleAAP", 1));
+    }
+    private static string createAAP(string paramName, float val)
+    {
+        var clipPath = $@"Assets/{paramName}.anim";
+        AnimationClip animationClip = new AnimationClip();
+        EditorCurveBinding curveBinding = EditorCurveBinding.FloatCurve("Animator", typeof(Animator), $"FloatParameter.{paramName}");
+        AnimationCurve curve = new AnimationCurve(new Keyframe(0, val));
+        AnimationUtility.SetEditorCurve(animationClip, curveBinding, curve);
+        AssetDatabase.CreateAsset(animationClip, clipPath);
+        return clipPath;
+    }
+}
